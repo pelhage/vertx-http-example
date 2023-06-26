0f8ef20240babdd3e7383bf1ab066362742712fc
Patrick El-Hage <patrick.elhage@gmail.com>
2023-06-25 20:14:54 -0700

    playing around with more route declarations

--- src/main/kotlin/com/example/starter/MainVerticle.kt
+++ src/main/kotlin/com/example/starter/MainVerticle.kt
@@ -1,12 +1,12 @@
 package com.example.starter
 
 import io.vertx.core.AbstractVerticle
+import io.vertx.core.http.HttpMethod
+import io.vertx.ext.web.Router
+import io.vertx.ext.web.RoutingContext
+import io.vertx.ext.web.handler.LoggerHandler
 import io.vertx.kotlin.core.json.json
 import io.vertx.kotlin.core.json.obj
-import io.vertx.ext.web.Router
-import io.vertx.ext.web.handler.LoggerHandler
-import io.vertx.ext.web.RoutingContext
-import com.example.starter.getFoo
 
 
 class MainVerticle : AbstractVerticle() {
@@ -15,16 +15,37 @@ class MainVerticle : AbstractVerticle() {
     val logger =  LoggerHandler.create()
     val router = Router.router(vertx)
     val exampleRepository = ExampleRepository()
+
+    // logging
     router.route().handler(logger);
 
-    // mount the handler for all incoming requests at every path and HTTP method
+    // hello route
+    router["/hello/*"]
+      .respond { ctx: RoutingContext ->
+        val path = ctx.request().path()
+        ctx
+          .response()
+          .putHeader("Content-Type", "text/plain")
+          .end("hello world! $path")
+      }
+
+    router.route(HttpMethod.GET, "/authors/:authorId")
+      .handler { ctx: RoutingContext ->
+        val authorId = ctx.pathParam("authorId")
+        ctx.response()
+          .putHeader("Content-Type", "text/plain")
+          .end("hello $authorId")
+    }
+
+    router.route().method(HttpMethod.POST)
+
+    // catch-all: mount the handler for all incoming requests at every path and HTTP method
     router.route().handler { context: RoutingContext ->
       // Get the address of the request
       val address = context.request().connection().remoteAddress()
       // get the query parameter 'name'
       val queryParams = context.queryParams()
       val name = queryParams.get("name") ?: "unknown"
-
       context.json(
         json {
           obj(
